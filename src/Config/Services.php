addf17f3a78d7bd3cccd7d59bd49b21e3a21a5fc
Rafael Pinheiro <rafaelwendel@hotmail.com>
2026-06-27 22:11:38 -0300

    Edit alerts method to define library config and return a new instance of Alerts

--- src/Config/Services.php
+++ src/Config/Services.php
@@ -13,18 +13,33 @@ use CI4Alerts\Alerts;
 class Services extends BaseService
 {
     /**
-     * Returns the authentication service instance.
-     *
+     * @param string|null $library The library config to retrieve.
      * @param bool $getShared Whether to return a shared instance.
      *
      * @return Alerts
      */
-    public static function alerts(bool $getShared = false)
+    public static function alerts(?string $library = null, bool $getShared = true)
     {
-        if ($getShared) {
-            return static::getSharedInstance('alerts');
+        $configObj = config('Alerts');
+
+        if ($library === null) {
+            $library = $configObj->active ?? 'bulma';
         }
 
-        return new Alerts();
+        if ($getShared) {
+            return static::getSharedInstance('alerts', $library);
+        }
+
+        $config = [];
+        if ($configObj) {
+            if (isset($configObj->config[$library]) && is_array($configObj->config[$library])) {
+                $config = $configObj->config[$library];
+            } elseif (isset($configObj->config) && is_array($configObj->config)) {
+                // Backwards compatibility with flat config files
+                $config = $configObj->config;
+            }
+        }
+
+        return new Alerts($config);
     }
 }
